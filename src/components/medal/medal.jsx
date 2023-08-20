18b663404665b5c6f26c503e5aaea303055449e5
Sana Sreeraj <iamsanasreeraj@gmail.com>
2023-08-20 06:04:30 +0530

    Map and Medal

--- src/components/medal/medal.jsx
+++ src/components/medal/medal.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
-import Map from '../map/map'
+import React from 'react';
 
 const Medal = () => {
   return (
-    <div>
-        <Map/>
+    <div className="bg-gray-100 min-h-screen py-10">
+      <div className="text-center">
+        <h1 className="text-3xl font-semibold mb-4">Redeem</h1>
+        <img src="" alt="" />
+      </div>
+      {/* Box 1 */}
+      <div className="bg-white p-4 rounded-md shadow-md my-6 mx-auto w-80">
+        <h2 className="text-xl font-semibold mb-2">Free pass for cultural fest</h2>
+        <p className="text-gray-600 mb-4">Join the fun at Island Grounds, Chennai on 20th August 12 PM onwards</p>
+        <button className="bg-black text-white px-4 py-2 rounded-full">20 points</button>
+      </div>
+      {/* Box 2 */}
+      <div className="bg-white p-4 rounded-md shadow-md my-6 mx-auto w-80">
+        <h2 className="text-xl font-semibold mb-2">5% on Bookmyshow</h2>
+        <p className="text-gray-600 mb-4">Get 5% off upto INR50 on selected movies</p>
+        <button className="bg-black text-white px-4 py-2 rounded-full">10 points</button>
+      </div>
+      {/* Box 3 */}
+      <div className="bg-white p-4 rounded-md shadow-md my-6 mx-auto w-80">
+        <h2 className="text-xl font-semibold mb-2">10% on Myntra</h2>
+        <p className="text-gray-600 mb-4">Get upto 10% off upto 250rs on select merchandise</p>
+        <button className="bg-black text-white px-4 py-2 rounded-full">30 points</button>
+      </div>
+      {/* Box 4 */}
+      <div className="bg-white p-4 rounded-md shadow-md my-6 mx-auto w-80">
+        <h2 className="text-xl font-semibold mb-2">Uber ride discount</h2>
+        <p className="text-gray-600 mb-4">Get Flat 50rs on your uber city rides</p>
+        <button className="bg-black text-white px-4 py-2 rounded-full">10 points</button>
+      </div>
+    const tabItems = [
+    
+  ];
     </div>
-  )
-}
+  );
+};
 
-export default Medal
+export default Medal;
